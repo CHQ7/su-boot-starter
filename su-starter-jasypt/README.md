260a4d53ee956df391d0cf863e4b2617358b1d9a
JsckChin <1259900@qq.com>
2022-03-08 10:32:26 +0800

    增强功能，并编写说明文档 https://gitee.com/umb/su-starter/issues/I4WRW7

--- su-starter-jasypt/README.md
+++ su-starter-jasypt/README.md
@@ -21,6 +21,65 @@ Jasypt是一个加密库对配置文件进行加密
 | lombok |      |
 | jasypt-spring-boot-starter     |   Jasypt安全框架加密组件  |
 
+# 使用方式
+
+# 方式一：通过工具类调用
+```java
+public class test {
+    public static void main(String[] args) {
+        // 待加密字符串
+        String txt = "abcd";
+        // 加密
+        String ciphertext = JasyptUtil.encrypt(txt);
+        // 解密
+        String decrypttext = JasyptUtil.decrypt(ciphertext);
+        // 打印
+        System.out.printf("原文:%s%n密文:%s%n解密:%s%n", txt, ciphertext, decrypttext);
+    }
+}
+```
+
+# 方式二：服务调用
+```java
+public class test {
+    
+    @Autowired
+    StringEncryptor stringEncryptor;
+
+    public void a() {
+        // 待加密字符串
+        String txt = "abcd";
+        // 加密 
+        String ciphertext = stringEncryptor.encrypt(txt);
+        // 解密
+        String decrypttext = stringEncryptor.decrypt(ciphertext);
+        // 打印
+        System.out.printf("原文:%s%n密文:%s%n解密:%s%n", txt, ciphertext, decrypttext);
+    }
+}
+```
+
+# 配置文件加密
+
+* 首先通过上面方式加密的字符串，添加到配置文件中，那么我们程序Jasypt在启动的时候会自动解析出明文
+
+
+application.yml
+```
+##############################################
+# spring
+##############################################
+spring:
+  # 数据源配置
+  datasource:
+    driver-class-name: com.mysql.cj.jdbc.Driver
+    url: jdbc:mysql://127.0.0.1:3306/test?serverTimezone=GMT%2B8&useSSL=false&useUnicode=true&characterEncoding=UTF-8&allowPublicKeyRetrieval=true
+    # 在使用的时候，您要使用ENC限定符字眼，告诉jasypt需要解析他
+    username: ENC(x0jY0g478sC7qMnAMOmvgA==)
+    password: ENC(5YfhKmsp4Ugsq70ookGQfw==)
+```
+
+
 # 更新日志
 
 2022-02-16
--- su-starter-jasypt/src/main/java/com/yunqi/starter/jasypt/utils/JasyptUtil.java
+++ su-starter-jasypt/src/main/java/com/yunqi/starter/jasypt/utils/JasyptUtil.java
@@ -9,40 +9,12 @@ import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
  */
 public class JasyptUtil {
 
-    /**
-     * <p>
-     *     加密
-     * </p>
-     *
-     * @param salt      加密盐值,配置文件中设定的加密密码 jasypt.encryptor.password
-     * @param value     待加密值
-     * @return          数字签名
-     */
-    public static String encrypt(String salt, String value) {
-        PooledPBEStringEncryptor encryptOr = new PooledPBEStringEncryptor();
-        encryptOr.setConfig(cryptOr(salt));
-        return encryptOr.encrypt(value);
-    }
+    /** 默认加密盐值 */
+    public static final String DEFAULT_JASYPT_SALT = "egsnhm";
 
     /**
-     * <p>
-     *     解密
-     * </p>
+     * Jasypt配置
      *
-     * @param salt     加密盐值,配置文件中设定的加密密码 jasypt.encryptor.password
-     * @param value    待解密密文
-     * @return         解密原文
-     */
-    public static String decypt(String salt, String value) {
-        PooledPBEStringEncryptor encryptOr = new PooledPBEStringEncryptor();
-        encryptOr.setConfig(cryptOr(salt));
-        return encryptOr.decrypt(value);
-    }
-
-    /**
-     * <p>
-     *     Jasypt配置
-     * </p>
      * @param salt 加密盐值
      * @return     Jasypt配置
      */
@@ -57,10 +29,53 @@ public class JasyptUtil {
         return config;
     }
 
-
-    public static void main(String[] args) {
-        String txt = "root";
-        String ciphertext = JasyptUtil.encrypt("egsnhm", txt);
-        System.out.printf("原文:%s%n密文:%s%n解密:%s%n", txt, ciphertext, JasyptUtil.decypt("egsnhm",ciphertext));
+    /**
+     * 加密
+     *
+     * @param salt      加密盐值,配置文件中设定的加密密码 jasypt.encryptor.password
+     * @param value     待加密值
+     * @return          数字签名
+     */
+    public static String encrypt(String salt, String value) {
+        PooledPBEStringEncryptor encryptOr = new PooledPBEStringEncryptor();
+        encryptOr.setConfig(cryptOr(salt));
+        return encryptOr.encrypt(value);
     }
+
+    /**
+     * 加密
+     *
+     * @param value     待加密值
+     * @return          数字签名
+     */
+    public static String encrypt(String value) {
+        return encrypt(DEFAULT_JASYPT_SALT, value);
+    }
+
+
+    /**
+     * 解密
+     *
+     * @param salt     加密盐值,配置文件中设定的加密密码 jasypt.encryptor.password
+     * @param value    待解密密文
+     * @return         解密原文
+     */
+    public static String decrypt(String salt, String value) {
+        PooledPBEStringEncryptor encryptOr = new PooledPBEStringEncryptor();
+        encryptOr.setConfig(cryptOr(salt));
+        return encryptOr.decrypt(value);
+    }
+
+
+    /**
+     * 解密
+     *
+     * @param value     待解密密文
+     * @return          解密原文
+     */
+    public static String decrypt(String value) {
+        return  decrypt(DEFAULT_JASYPT_SALT, value);
+    }
+
+
 }
