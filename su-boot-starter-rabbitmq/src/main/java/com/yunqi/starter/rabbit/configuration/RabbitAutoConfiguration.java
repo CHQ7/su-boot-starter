8ea38916ba7923e7364894945a00916c6eba9e38
CHQ <1259900@qq.com>
2022-09-12 23:50:56 +0800

    :sparkles: 新增RabbitMQ组件

---
+++ su-boot-starter-rabbitmq/src/main/java/com/yunqi/starter/rabbit/configuration/RabbitAutoConfiguration.java
@@ -0,0 +1,58 @@
+package com.yunqi.starter.rabbit.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+
+
+/**
+ * RabbitMQ
+ * Created by @author CHQ on 2022/9/12
+ */
+@Slf4j
+@Configuration
+public class RabbitAutoConfiguration {
+
+    /**
+     * 创建队列
+     * @return 构造一个新队列
+     */
+    @Bean
+    public Queue queue() {
+        // 是否持久化
+        boolean durable = true;
+        // 仅创建者可以使用的私有队列，断开后自动删除
+        boolean exclusive = false;
+        // 当所有消费客户端连接端库后，是否自动删除队列
+        boolean autoDelete = false;
+        //一般设置一下队列的持久化就好,其余两个就是默认false
+        return new Queue(RabbitConfig.QUEUE_NAME, durable, exclusive, autoDelete);
+    }
+
+    /**
+     * 创建Direct交换器
+     * @return  构造一个新交换器
+     */
+    @Bean
+    public DirectExchange exchange() {
+        boolean durable = true;
+        boolean autoDelete = false;
+        return new DirectExchange(RabbitConfig.EXCHANGE,durable,autoDelete);
+    }
+
+
+    /**
+     * 绑定交换器和队列通过路由键
+     * @return  绑定交换器和队列
+     */
+    @Bean
+    public Binding bindingDirect() {
+        return BindingBuilder.bind(queue()).to(exchange()).with(RabbitConfig.ROUTEKEY);
+    }
+
+}
---
+++ su-boot-starter-rabbitmq/src/main/java/com/yunqi/starter/rabbit/configuration/RabbitConfig.java
@@ -0,0 +1,19 @@
+package com.yunqi.starter.rabbit.configuration;
+
+
+/**
+ * RabbitMQ
+ * Created by @author CHQ on 2022/9/12
+ */
+public class RabbitConfig {
+
+    /** 队列名称 */
+    public static final String QUEUE_NAME = "DirectQueue";
+
+    /** 交换器名称 */
+    public static final String EXCHANGE  = "DirectExchange";
+
+    /** 路由键名称 */
+    public static final String ROUTEKEY  = "DirectRoutekey";
+
+}
