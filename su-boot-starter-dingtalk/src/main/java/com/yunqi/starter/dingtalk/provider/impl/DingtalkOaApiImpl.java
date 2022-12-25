462b3c3236134e1885d7dc4f2892728c2d0bdb1a
牟婧瑶 <10858694+suzymu@user.noreply.gitee.com>
2022-12-25 03:25:38 +0000

    OA审批模块服务实现类
    Signed-off-by: 牟婧瑶 <10858694+suzymu@user.noreply.gitee.com>

---
+++ su-boot-starter-dingtalk/src/main/java/com/yunqi/starter/dingtalk/provider/impl/DingtalkOaApiImpl.java
@@ -0,0 +1,23 @@
+package com.yunqi.starter.dingtalk.provider.impl;
+
+import com.yunqi.starter.common.lang.Lang;
+import com.yunqi.starter.common.lang.util.NutMap;
+import com.yunqi.starter.dingtalk.provider.IDingtalkOaApi;
+import com.yunqi.starter.dingtalk.util.DingtalkUtil;
+
+
+public class DingtalkOaApiImpl implements IDingtalkOaApi {
+
+
+    @Override
+    public String workflowForms(NutMap data) {
+        NutMap res = DingtalkUtil.post2("/v1.0/workflow/processInstances" + DingtalkUtil.buildToken(), data);
+        return res.getString("instanceId");
+    }
+
+    @Override
+    public NutMap workflowProcesses(String processInstanceId) {
+        NutMap res = DingtalkUtil.get2("/v1.0/workflow/processInstances?processInstanceId="+processInstanceId);
+        return Lang.map(res.getString("result"));
+    }
+}
