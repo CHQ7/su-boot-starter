6b05b50f396f16664ebc93d3a19ba9228d9ae108
CHQ <1259900@qq.com>
2023-02-09 17:08:49 +0800

    优化: 说明文档

--- su-boot-starter-mail/src/main/java/com/yunqi/starter/mail/configuration/MailProperties.java
+++ su-boot-starter-mail/src/main/java/com/yunqi/starter/mail/configuration/MailProperties.java
@@ -17,18 +17,25 @@ public class MailProperties {
     /** 是否开启 */
     boolean enabled = true;
 
+    /** 邮件服务器地址 */
     private String hostName = "smtp.qiye.aliyun.com";
 
+    /** 邮件服务器端口 */
     private String smtpPort = "465";
 
+    /** 邮箱账号 */
     private String userName = "notice@fir7.com";
 
+    /** 邮箱密码 */
     private String password = "Ng1259900";
 
+    /** 邮件服务器是否HTTPS */
     private boolean ssl = true;
 
+    /** 邮箱用户名 */
     private String from = "notice@fir7.com";
 
+    /** 邮箱编码 */
     private String charset = "UTF-8";
 
 }
